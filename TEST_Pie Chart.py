9cd6130d7acf91c8530b42eb707e39e33b6e60d8
MIN-1122 <mjc04101618@gmail.com>
2026-05-28 01:31:22 +0800

    test_Pie Chart

--- TEST_Pie Chart.py
+++ TEST_Pie Chart.py
@@ -2,7 +2,10 @@ import matplotlib.pyplot as plt
 plt.rc("font",family="Microsoft YaHei")
 
 plt.pie([20,30,40],
-        labels=["第一塊","第二塊","第三塊"])   #資料點
+        labels=["第一塊","第二塊","第三塊"],
+        labeldistance=0.5)   #資料點
+
+plt.title("這是一個圓餅圖")
 
 plt.legend()
 plt.show()
