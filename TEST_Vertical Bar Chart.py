ca07763c3e4be5ef9789d96128c908a03f9ee337
MIN-1122 <mjc04101618@gmail.com>
2026-06-09 22:15:35 +0800

    test_Vertical Bar Chart

---
+++ TEST_Vertical Bar Chart.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+plt.rc("font",family="Microsoft YaHei")
+
+#資料點(x,height):("T",4) ("A",6) ("O",2)
+
+plt.bar(
+    ["T","A","O"],
+    [4,6,2],
+    width=0.5,         #width設定參數寬度
+    color="red"
+)
+
+plt.xlabel("x軸")
+plt.ylabel("y軸")
+
+plt.legend()
+plt.show()
