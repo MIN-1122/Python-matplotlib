7ce5bb7e4f1f570ac6581e5eb3540e4de1b97110
MIN-1122 <mjc04101618@gmail.com>
2026-06-09 21:23:22 +0800

    test_Pie Chart

--- TEST_Pie Chart.py
+++ TEST_Pie Chart.py
@@ -6,10 +6,10 @@ total=sum(x)    #sum(列表)，計算列表中的數值總和
 labels={str(100*data/total)+"%" for data in x}     #產生列表並計算%值
 
 plt.pie(
-        x,
+        x,                               #資料點
         labels=labels,
         labeldistance=0.5,               #參數設定標籤位置
-)   #資料點
+)
 
 plt.title("這是一個圓餅圖")            #圖表標題
 
